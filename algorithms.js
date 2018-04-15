d8f302403ad77005b156b76566a51f5cca229a5b
Aileen Wong <aileen.m.wong@gmail.com>
2018-04-15 18:00:57 -0400

    reverse a string

--- algorithms.js
+++ algorithms.js
@@ -5,10 +5,26 @@
 //use join array method to print array as a string
 function printArr() {
   const input = ['sam','ed','harry'];
+  //return the items in array as a joined string
   return input.join();
 }
-
 console.log(printArr());
 
+//REVERSE A STRING
+//Write a function called reverseString that will take a string and return the string reversed.
+//Input: 'abcd'
+//Output: 'dcba’
 
+function reverseString(string) {
+  //newString variable is an empty array
+  let newString = [];
+  //loop through string backwards and push into empty array
+   for (let i=string.length-1; i>=0; i--) {
+     newString.push(string[i]);
 
+   }
+   //join string back together
+  return newString.join('');
+}
+
+reverseString('abcd');
