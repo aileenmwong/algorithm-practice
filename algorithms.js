24fc4e58440df15b1afe06ffeda1853bfb0af164
Aileen Wong <aileen.m.wong@gmail.com>
2018-04-15 17:32:49 -0400

    print an array algorithm

---
+++ algorithms.js
@@ -0,0 +1,14 @@
+//PRINT AN ARRAY
+//Write a function called printArr that will print the items of an array.
+//Input: ['sam','ed','harry']
+
+//use join array method to print array as a string
+function printArr() {
+  const input = ['sam','ed','harry'];
+  return input.join();
+}
+
+console.log(printArr());
+
+
+
